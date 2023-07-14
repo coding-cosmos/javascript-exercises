0555c50ffe97d6c34eabce02176fb3a66baa1727
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-07-14 15:22:12 +0530

    Add sum functionality

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(numOne,numTwo) {
+    
+    if (numOne<0 || numTwo<0||isNaN(numOne)||isNaN(numTwo)||typeof(numOne)==="string"||typeof(numTwo)==="string") {
+        return "ERROR";
+    }
+   let min = Math.min(numOne,numTwo);
+   let max = Math.max(numOne,numTwo);
+   let sum = 0;
+   for (let num = min ; num <= max; num++) {
+       sum+=num;
+   }
+   return sum;
+}
+sumAll(1,"5");
 // Do not edit below this line
 module.exports = sumAll;
