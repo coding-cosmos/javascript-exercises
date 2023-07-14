653f5856487a09e79b4917cf15f3ff750cee7e52
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-07-14 13:45:00 +0530

    Add required functionality

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,33 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+   let words = string.split(" ");
+    reverse(words);
+   return(join(words));
 };
 
+function reverse(words){
+    
+    for (let wordNo = 0; wordNo < words.length; wordNo++) {
+        let reverseWord ="";
+        for (let wordLength = words[wordNo].length-1; wordLength>=0; wordLength--) {
+            reverseWord += words[wordNo][wordLength];
+        }
+        words[wordNo] = reverseWord;
+    }
+    
+    return words;
+}
+
+function join(array){
+   let finalString ="";
+    for (let index = array.length-1; index >=0; index--) {
+       if (index === 0) {
+        finalString += array[index];
+       } else {
+        finalString += array[index]+" ";
+       }
+    }
+    return finalString;
+}
+reverseString("Hello there");
 // Do not edit below this line
 module.exports = reverseString;
