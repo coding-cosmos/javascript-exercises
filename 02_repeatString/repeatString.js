0701e0acc71435aa3cb5629c277b8d591c26cbd5
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-07-14 12:46:34 +0530

    Add required functionality

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string , num) {
+    let finalString="";
+    if (num<0) {
+        return "ERROR";
+    }
+   for (let index = 0; index < num; index++) {
+    finalString += string;
+   }
+   return finalString;
 };
 
 // Do not edit below this line
