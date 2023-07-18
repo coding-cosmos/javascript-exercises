138f60f86ee6438b609f589ce9815210b66208a6
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-07-18 20:26:51 +0530

    Add functionality for different function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+return	arr.reduce((sum,item)=>{
+    return sum + item;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let result = 1;
+  for(let arg of args){
+    result *= arg;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fac = 1;
+	for (let i = n; i > 0; i--) {
+    fac *= i
+  }
+  return fac;
 };
 
 // Do not edit below this line
